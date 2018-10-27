f0a9bef299b6d8b9355d73828bb70291c1d425f6
sushemsu <sushemsu@gmail.com>
2018-10-27 10:30:01 -0600

    initial commit

---
+++ test_tcp.go
@@ -0,0 +1,28 @@
+package main
+import (
+    "fmt"
+    "net"
+    "time"
+    "os"
+);
+func helpcontext(error,bin string){
+	fmt.Printf("%s\n%s hostname[port]\n",error, bin);
+	os.Exit(2);
+};
+func main(){
+	port:="80";
+        if len(os.Args) == 1 {
+		helpcontext("Expected host/ip", os.Args[0]);
+	}
+        if len(os.Args) == 3  {
+		port=os.Args[2];
+	}
+        start := time.Now();
+        _, error := net.Dial("tcp", os.Args[1]+":"+port);
+        spent := time.Now();
+	if error != nil {
+		fmt.Printf("error: %s\n", error);
+		os.Exit(1);
+	}
+	fmt.Printf("Connect time: %d\n", spent.Sub(start));
+};
